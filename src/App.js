1a4daa95bba32ecb711a514eca032ba02f9db208
Ruben <ruben@publicinput.com>
2023-01-25 16:54:51 -0500

    implement the red wine api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import {getAllRedWines} from './ApiService';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -21,20 +24,32 @@ function App() {
 
   return (
     <Container className='App'>
-      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Row>
+        <div className='App-header'>Red Wine Gallery</div>
+        <Row xs={1} md={3} lg={4} xl={5}>
           {redWines.map((wine) => {
             return (
-              <Col className='red'key={wine.id}>
-                <div className='border border-danger'>
-                  <p>{wine.wine}</p>
-                </div>
+              <Col className='p-3' key={wine.id}>
+                {console.log(wine)}
+                <Card className='h-100'>
+                  <div className='text-center'>
+                    <Image fluid src={wine.image} />
+                  </div>
+                  <Card.Body>
+                    <Card.Title>{wine.wine}</Card.Title>
+                    <Card.Text>
+                      Location: {wine.location}
+                      <br/>
+                      Rating: {wine.rating.average}
+                      <br/>
+                      Reviews: {wine.rating.reviews}
+                      </Card.Text>
+                  </Card.Body>
+                </Card>
               </Col>
             )
           })}
         </Row>
-      </header>
     </Container>
   );
 }
