a911fb4f43d8a4585e398bc464791dab190cdb65
Ruben <ruben@publicinput.com>
2023-01-27 12:35:42 -0500

    ready for release

--- index.js
+++ index.js
@@ -7,12 +7,3 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
-
-// create a GET route
-app.get('/', (req, res) => { //Line 9
-  res.send({ express: "You're calling froggy 101" }); 
-}); 
-
-app.get('/react', (req, res) => { //Line 9
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
-}); 
