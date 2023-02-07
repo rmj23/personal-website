093b2a16ddbbeb3993d6c0e1c5136f141df840c7
Ruben <ruben@publicinput.com>
2023-02-07 14:43:06 -0500

    final wine design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,35 +8,40 @@ import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
 import Form from 'react-bootstrap/Form';
 
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [redWines, setRedWines] = useState([]);
+  const [allWines, setAllWines] = useState([]);
 
   useEffect(() => {
     const fetchRedWines = async () => {
       const wines = await getAllRedWines();
       setRedWines(wines);
+      setAllWines(wines);
     }
 
     fetchRedWines();
   }, []);
 
+  function Search(searchTerm) {
+    var results = allWines.filter(wine => wine.location.toLowerCase().includes(searchTerm) ||  wine.wine.toLowerCase().includes(searchTerm));
+    setRedWines(results);
+  }
+
   return (
     <Container className='App'>
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className='App-header'>Red Wine Gallery</div>
+        <h1 className='App-header m-5'>Red Wine Gallery</h1>
         <Form className='pull-right'>
           <Form.Group>
-            <Form.Control type='text' placeholder='Search for a wine!'></Form.Control>
+            <Form.Control type='text' placeholder='Search for a wine!' onChange={e => Search(e.target.value)} />
+            <h3 className='p-2'>{redWines.length} wines!!!</h3>
           </Form.Group>
         </Form>
         <Row xs={1} md={3} lg={4} xl={5}>
           {redWines.map((wine) => {
             return (
               <Col className='p-3' key={wine.id}>
-                {console.log(wine)}
                 <Card className='h-100'>
                   <div className='text-center'>
                     <Image fluid src={wine.image} />
